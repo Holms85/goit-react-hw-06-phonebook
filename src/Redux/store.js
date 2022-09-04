7727846702136395493dc6533bd230b6a57ae236
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-09-04 22:32:04 +0300

    add redux-toolkit

---
+++ src/Redux/store.js
@@ -0,0 +1,17 @@
+// import { configureStore, createSlice, createAction, createReducer } from "@reduxjs/toolkit";
+
+
+// const mySlice = createSlice({
+//     name: 'contact',
+//     initialState: [],
+//     reducers: {
+//         addContacts(state, action) {
+//             return state.push(action.payload);
+//         }
+//     }
+// })
+
+
+// export const store = configureStore({
+//     items: {},
+// })
